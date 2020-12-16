3ce47490f035ace899945451d21291c348d519ab
VilleJyva <72099025+VilleJyva@users.noreply.github.com>
2020-12-16 15:12:01 +0200

    Start of real development

--- WPDirectory.php
+++ WPDirectory.php
@@ -1,3 +1,19 @@
-/**
- * Plugin Name: WP Directory
+<?php /**
+ * Plugin Name:       WPDirectory
+ * Plugin URI:        https://github.com/VilleJyva/WPDirectory.git
+ * Description:       Simple WP Directory Plugin
+ * Version:           0.0.1
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Ville Siren
+ * Author URI:        https://jyva.io/
+ * License:           All rights reserved
+ * Text Domain:       wpdirectory
+ * Domain Path:       /languages
  */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+      
